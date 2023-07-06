bdeefd2eb980455b1aa7b208652c9f523ce81b22
gabrielhrp-16 <gabrielhrp16@gmail.com>
2023-07-06 15:45:01 -0300

    exclusão de trechos desnecessários

--- TriageSystem/src/exception.cpp
+++ TriageSystem/src/exception.cpp
@@ -22,26 +22,3 @@ const char *SequenciaInvalidaEspacos::what() const noexcept {
     return "Número muito grande de espaços ( >4 consecutivos ), tente novamente!";
 }
 
-const char *FormatoInvalido::what() const noexcept {
-    return "Formato Inválido, tente novamente!";
-}
-
-const char* TemperaturaInvalida::what() const noexcept {
-    return "Temperatura inválida. O valor deve estar acima de 0°C.";
-}
-
-const char* AlturaInvalida::what() const noexcept {
-    return "Altura inválida. O valor deve estar entre 0m e 3m.";
-}
-
-const char* PesoInvalido::what() const noexcept {
-    return "Peso inválido. O valor deve estar entre 0 e 500kg.";
-}
-
-const char* FreqCardiacaInvalida::what() const noexcept {
-    return "Frequência Cardíaca inválida. O valor deve ser um número inteiro positivo.";
-}
-
-const char* PressaoInvalida::what() const noexcept {
-    return "Pressão inválida. Digite no formato 'xx/xx' ou 'xxx/xxx'.";
-}
--- TriageSystem/src/exception.hpp
+++ TriageSystem/src/exception.hpp
@@ -29,33 +29,5 @@ public:
     const char *what() const noexcept override;
 };
 
-class FormatoInvalido : public std::exception {
-public:
-    const char *what() const noexcept override;
-};
 
-class TemperaturaInvalida : public std::exception {
-public:
-    const char* what() const noexcept override;
-};
-
-class AlturaInvalida : public std::exception {
-public:
-    const char* what() const noexcept override;
-};
-
-class PesoInvalido : public std::exception {
-public:
-    const char* what() const noexcept override;
-};
-
-class FreqCardiacaInvalida : public std::exception {
-public:
-    const char* what() const noexcept override;
-};
-
-class PressaoInvalida : public std::exception {
-public:
-    const char* what() const noexcept override;
-};
 #endif
