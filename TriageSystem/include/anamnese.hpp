22901cb9465f534a4476787348fbef79b3400b30
Hector Roberto <hectorroberto@ufmg.br>
2023-07-07 21:23:04 -0300

    Anamnese doxygen

--- TriageSystem/include/anamnese.hpp
+++ TriageSystem/include/anamnese.hpp
@@ -2,32 +2,98 @@
 #define ANAMNESE_HPP
 
 #include <string>
-
+/**
+ * @brief Construtor da classe Anamnese.
+ * @param sintomas Os sintomas informados na anamnese.
+ * @param temperatura A temperatura informada na anamnese.
+ * @param altura A altura informada na anamnese.
+ * @param peso O peso informado na anamnese.
+ * @param freq_cardiaca A frequência cardíaca informada na anamnese.
+ * @param pressao A pressão informada na anamnese.
+ */
 class Anamnese {
 public:
     Anamnese();
     Anamnese(const std::string& sintomas, float temperatura, float altura, float peso, unsigned freq_cardiaca, const std::string& pressao);
-
+    /**
+     * @brief Obtém os sintomas informados na anamnese.
+     * @return Os sintomas da anamnese.
+     */
     std::string verSintomas() const;
+    /**
+     * @brief Edita os sintomas da anamnese.
+     * @param sintomas Os novos sintomas a serem atribuídos.
+     */
     void editarSintomas(const std::string &sintomas);
 
+    /**
+     * @brief Obtém a temperatura informada na anamnese.
+     * @return A temperatura da anamnese.
+     */
     float verTemperatura() const;
-    void editarTemperatura(float temperatura);
 
+    /**
+     * @brief Edita a temperatura da anamnese.
+     * @param temperatura A nova temperatura a ser atribuída.
+     */
+    void editarTemperatura(float temperatura);
+    /**
+     * @brief Obtém a altura informada na anamnese.
+     * @return A altura da anamnese.
+     */
     float verAltura() const;
+
+    /**
+     * @brief Edita a altura da anamnese.
+     * @param altura A nova altura a ser atribuída.
+     */
     void editarAltura(float altura);
 
+    /**
+     * @brief Obtém o peso informado na anamnese.
+     * @return O peso da anamnese.
+     */
     float verPeso() const;
+
+    /**
+     * @brief Edita o peso da anamnese.
+     * @param peso O novo peso a ser atribuído.
+     */
     void editarPeso(float peso);
 
+    /**
+     * @brief Obtém a frequência cardíaca informada na anamnese.
+     * @return A frequência cardíaca da anamnese.
+     */
     unsigned verFreq_Cardiaca() const;
+
+    /**
+     * @brief Edita a frequência cardíaca da anamnese.
+     * @param freq_cardiaca A nova frequência cardíaca a ser atribuída.
+     */
     void editarFreq_Cardiaca(unsigned freq_cardiaca);
 
+    /**
+     * @brief Obtém a pressão informada na anamnese.
+     * @return A pressão da anamnese.
+     */
     std::string verPressao() const;
+
+    /**
+     * @brief Edita a pressão da anamnese.
+     * @param pressao A nova pressão a ser atribuída.
+     */
     void editarPressao(const std::string &pressao);
 
+    /**
+     * @brief Visualiza os dados da anamnese.
+     */
     void visualizarAnamnese() const;
-
+    
+    /**
+     * @brief Coleta os dados da anamnese.
+     * @return Uma instância de Anamnese preenchida com os dados coletados.
+     */
     static Anamnese coletarDadosAnamnese();
 
 private:
--- TriageSystem/src/anamnese.cpp
+++ TriageSystem/src/anamnese.cpp
@@ -8,15 +8,7 @@
  */
 Anamnese::Anamnese() {}
 
-/**
- * @brief Construtor da classe Anamnese.
- * @param sintomas Os sintomas informados na anamnese.
- * @param temperatura A temperatura informada na anamnese.
- * @param altura A altura informada na anamnese.
- * @param peso O peso informado na anamnese.
- * @param freq_cardiaca A frequência cardíaca informada na anamnese.
- * @param pressao A pressão informada na anamnese.
- */
+
 Anamnese::Anamnese(const std::string &sintomas, float temperatura, float altura, float peso, unsigned freq_cardiaca, const std::string &pressao) {
     _sintomas = sintomas;
     _temperatura = temperatura;
@@ -26,105 +18,67 @@ Anamnese::Anamnese(const std::string &sintomas, float temperatura, float altura,
     _pressao = pressao;
 }
 
-/**
- * @brief Obtém os sintomas informados na anamnese.
- * @return Os sintomas da anamnese.
- */
+
 std::string Anamnese::verSintomas() const {
     return _sintomas;
 }
 
-/**
- * @brief Edita os sintomas da anamnese.
- * @param sintomas Os novos sintomas a serem atribuídos.
- */
+
 void Anamnese::editarSintomas(const std::string &sintomas) {
     _sintomas = sintomas;
 }
 
-/**
- * @brief Obtém a temperatura informada na anamnese.
- * @return A temperatura da anamnese.
- */
+
 float Anamnese::verTemperatura() const {
     return _temperatura;
 }
 
-/**
- * @brief Edita a temperatura da anamnese.
- * @param temperatura A nova temperatura a ser atribuída.
- */
+
 void Anamnese::editarTemperatura(float temperatura) {
     _temperatura = temperatura;
 }
 
-/**
- * @brief Obtém a altura informada na anamnese.
- * @return A altura da anamnese.
- */
+
 float Anamnese::verAltura() const {
     return _altura;
 }
 
-/**
- * @brief Edita a altura da anamnese.
- * @param altura A nova altura a ser atribuída.
- */
+
 void Anamnese::editarAltura(float altura) {
     _altura = altura;
 }
 
-/**
- * @brief Obtém o peso informado na anamnese.
- * @return O peso da anamnese.
- */
+
 float Anamnese::verPeso() const {
     return _peso;
 }
 
-/**
- * @brief Edita o peso da anamnese.
- * @param peso O novo peso a ser atribuído.
- */
+
 void Anamnese::editarPeso(float peso) {
     _peso = peso;
 }
 
-/**
- * @brief Obtém a frequência cardíaca informada na anamnese.
- * @return A frequência cardíaca da anamnese.
- */
+
 unsigned Anamnese::verFreq_Cardiaca() const {
     return _freq_cardiaca;
 }
 
-/**
- * @brief Edita a frequência cardíaca da anamnese.
- * @param freq_cardiaca A nova frequência cardíaca a ser atribuída.
- */
+
 void Anamnese::editarFreq_Cardiaca(unsigned freq_cardiaca) {
     _freq_cardiaca = freq_cardiaca;
 }
 
-/**
- * @brief Obtém a pressão informada na anamnese.
- * @return A pressão da anamnese.
- */
+
 std::string Anamnese::verPressao() const {
     return _pressao;
 }
 
-/**
- * @brief Edita a pressão da anamnese.
- * @param pressao A nova pressão a ser atribuída.
- */
+
 void Anamnese::editarPressao(const std::string &pressao) {
     _pressao = pressao;
 }
 
-/**
- * @brief Visualiza os dados da anamnese.
- */
+
 void Anamnese::visualizarAnamnese() const {
     std::cout << "Sintomas: " << _sintomas << std::endl;
     std::cout << "Temperatura: " << _temperatura << "°C" << std::endl;
@@ -134,10 +88,7 @@ void Anamnese::visualizarAnamnese() const {
     std::cout << "Pressão: " << _pressao << std::endl;
 }
 
-/**
- * @brief Coleta os dados da anamnese.
- * @return Uma instância de Anamnese preenchida com os dados coletados.
- */
+
 Anamnese Anamnese::coletarDadosAnamnese() {
     std::string sintomas;
     float temperatura;
